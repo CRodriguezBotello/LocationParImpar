be8604e21fea0d0f07d943f8139e4b87fe7fb8dc
Carlos Rodriguez <crbotello08@gmail.com>
2024-10-04 08:50:16 +0200

    arreglar fallos

--- par_impar.php
+++ par_impar.php
@@ -1,20 +1,18 @@
 <?php
     $num=$_GET["num"];
-    par_impar($num);
+    $par=par_impar($num);
+    $cuadrado=$num*$num;
     if($par){
         header("Location:par.php?cuadrado=$cuadrado&par=$num"); 
-    }elseif ($impar) {
+    }else{
         header("Location:impar.php?cuadrado=$cuadrado&impar=$num");
     }
 
     function par_impar($num){
-        $cuadrado=$num*$num;
         if ($num%2==0) {
-            $par=array(0,0,"0");
-            return $par($num, $cuadrado, "par");
+            return true;
         }else{
-            $impar=array(0,0,"0");
-            return $impar($num, $cuadrado, "impar");
+            return false;
         }
     }
 ?>
