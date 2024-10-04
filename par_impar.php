eca122fab7990abf1227a8109ba28debf025e302
Carlos Rodriguez <crbotello08@gmail.com>
2024-10-04 09:13:09 +0200

    cerrar comentario

--- par_impar.php
+++ par_impar.php
@@ -8,7 +8,7 @@
         header("Location:impar.php?cuadrado=$cuadrado&impar=$num");
     }
             /* La funcion par_impar averigua el resto del numero que entre en la funcion y 
-            devuelve true o false segun sea par o impar
+            devuelve true o false segun sea par o impar*/
     function par_impar($num){
         if ($num%2==0) {
             return true;
